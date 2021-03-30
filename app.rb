868ec37632c39bb992d4a277a209251e8b7a489f
Wonjin Cho <cwonjin11@gmail.com>
2021-03-29 23:06:16 -0400

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Wonjin Cho"
+    end
+
+
+    get '/hometown' do
+        "My hometown is Seoul, Korea"
+    end
+
+
+    get '/favorite-song' do
+        "My favorite song is baby shark"  
+    end
+
 end
